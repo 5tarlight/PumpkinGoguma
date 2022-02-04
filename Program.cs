90b2bbf0dcb55b354a413e9da46870499bea97fa
YEAHx4 <developerstarlight@gmail.com>
2022-02-04 21:42:59 +0900

    ask with up/down arrow

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace Hoguma
     static void Main(string[] args)
     {
       ConsoleUtil.WriteColor("\nWelcome To Pumpkin Potato\n", Colors.txtDefault);
+      ConsoleUtil.Pause();
+      ConsoleUtil.Ask(new List<string>() { "Start Game", "Exit" });
     }
   }
 }
