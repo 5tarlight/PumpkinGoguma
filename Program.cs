2a9648997e9b57093791a09fe70bf8f9d731dac6
hellun205 <hellun515@gmail.com>
2022-02-04 20:36:07 +0900

    format

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿class Program {
-    static void Main(string[] args) {
-        Console.WriteLine("Hello World!");
-    }
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello World!");
+  }
 }
+ 
